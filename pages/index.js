341a6874b0fced0ca88ef6ac3430795b0969b826
SahilPanhotra <sahilpanhotra@gmail.com>
2022-12-18 22:08:09 +0530

    build:setup eslint

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
 
-export default function Home() {
-  return (
-    <div >
-      <h1 className=' text-red-600'>HEY</h1>
-    </div>
-  )
-}
+const Home = () => (
+  <div>
+    <h1 className=" text-red-600">HEY</h1>
+  </div>
+);
+
+export default Home;

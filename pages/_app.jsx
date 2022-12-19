2b5d6c31839699b3379ae2a20612171284676e0f
SahilPanhotra <sahilpanhotra@gmail.com>
2022-12-19 19:24:54 +0530

    setup:all routes and fontawesome and basic componets required in all pages

---
+++ pages/_app.jsx
@@ -0,0 +1,19 @@
+import Script from 'next/script';
+import { ThemeProvider } from 'next-themes';
+
+import { Navbar, Footer } from '../components';
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <ThemeProvider attr="class">
+    <div className="dark:bg-nft-dark bg-white min-h-screen">
+      <Navbar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+
+    <Script src="https://kit.fontawesome.com/a9d6f93c97.js" crossorigin="anonymous" />
+  </ThemeProvider>
+);
+
+export default MyApp;

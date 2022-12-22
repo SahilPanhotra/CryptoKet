33fe9ebb8ea5a223f2a14bfb3dbd88feccc7c1f0
SahilPanhotra <sahilpanhotra@gmail.com>
2022-12-22 23:48:08 +0530

    build:NFT Cards & HomePage

--- components/index.js
+++ components/index.js
@@ -3,5 +3,6 @@ import Navbar from './Navbar';
 import Button from './Button';
 import Banner from './Banner';
 import CreatorCard from './CreatorCard';
+import NFTCard from './NFTCard';
 
-export { Button, Navbar, Footer, Banner, CreatorCard };
+export { Button, Navbar, Footer, Banner, CreatorCard, NFTCard };

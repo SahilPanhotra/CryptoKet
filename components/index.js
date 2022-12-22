2c770d4def005d2ee7ffc6d48c59c2e7460ce5fe
SahilPanhotra <sahilpanhotra@gmail.com>
2022-12-22 17:57:33 +0530

    build:Top sellers with Creator Component

--- components/index.js
+++ components/index.js
@@ -2,5 +2,6 @@ import Footer from './Footer';
 import Navbar from './Navbar';
 import Button from './Button';
 import Banner from './Banner';
+import CreatorCard from './CreatorCard';
 
-export { Button, Navbar, Footer, Banner };
+export { Button, Navbar, Footer, Banner, CreatorCard };
